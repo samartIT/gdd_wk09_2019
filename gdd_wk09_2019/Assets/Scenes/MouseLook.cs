f7e0520160cef98ca3287fa56b844c9804f95f03
FOR.FOSE <32724812+Apisit60070114@users.noreply.github.com>
2019-10-05 01:48:51 +0700

    update

---
+++ gdd_wk09_2019/Assets/Scenes/MouseLook.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes{
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityHor = 0.5f;
+    public float sensitivityVer = 0.5f;
+    public float minimumVer = -45.0f;
+    public float maximumVer = 45.0f;
+    public float _rotationX = 0;
+    private object thransfrom;
+
+    // Start is called before the first frame update
+    // Use this for initialization
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null)
+        {
+            body.freezeRotation = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (axes == RotationAxes.MouseX){
+            thransfrom.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+            //move horizontally, around Y-axis
+        } else if (axes == RotationAxes.MouseY){
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVer;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVer, maximumVer);
+
+            float rotationY = transform.localEulerAngles;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+            //move vertically, around X-axis
+        }
+        else{
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVer;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVer, maximumVer);
+
+            float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+            float rotationY = transform.localEulerAngles.y + delta;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+
+            //move both hor and vert around X,Y-axis
+        }
+    }
+}
